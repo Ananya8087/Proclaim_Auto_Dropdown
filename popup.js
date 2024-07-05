a154eb083ce2db6de006d7dc875277ae95db6ee6
Ananya V <90900377+Ananya8087@users.noreply.github.com>
2024-07-05 23:38:49 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,1 @@
-document.getElementById('submit-btn').addEventListener('click', () => {
-    const particular = document.getElementById('particular-input').value;
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { particular: particular });
-    });
-  });
-  
+
